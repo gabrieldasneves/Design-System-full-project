6fbaafa0c145424ff9b75420ef6634da70807317
Gabriel das Neves <galvesdasneves@gmail.com>
2022-10-12 23:34:56 -0300

    setup mininmal project
    using tailwind and postcss

---
+++ lab-ds/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+        <h1 className=' font-bold text-5xl text-violet-500'>Hello World!</h1>
+        
+        <button className='bg-violet-500 
+                          font-medium rounded
+                          text-white hover:bg-violet-700'>
+          Enviar
+        </button>
+    </>
+  )
+}
+
+
