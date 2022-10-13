30294754370713d0a01b3d03957966557e39d073
Gabriel das Neves <galvesdasneves@gmail.com>
2022-10-13 11:52:03 -0300

    setting up pattern tokens in tailwind file
    font size and colors

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -3,9 +3,9 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-        <h1 className=' font-bold text-5xl text-violet-500'>Hello World!</h1>
+        <h1 className=' font-bold text-2xl text-violet-500'>Hello World!</h1>
         
-        <button className='bg-violet-500 
+        <button className='bg-ciano-500
                           font-medium rounded
                           text-white hover:bg-violet-700'>
           Enviar
